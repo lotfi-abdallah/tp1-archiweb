c138b42033c41f8ba850fd56b2eb9d3465446249
Abdallah Lotfi <abdallahlotfi97@gmail.com>
2026-02-02 15:41:13 +0100

    use both http and https

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import https from 'https';
+import http from 'http';
 import fs from 'fs';
 
 const options = {
@@ -12,15 +13,17 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req: Request, res: Response) => {
-    // Respond with the index.html file located in the 'public' directory
+app.get('/', (_req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3188;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-https.createServer(options, app).listen(PORT, () => {
-    console.log(`HTTPS Server is running on port ${PORT}`);
+const HTTP_PORT = 3188;
+const HTTPS_PORT = 3189;
+
+http.createServer(app).listen(HTTP_PORT, () => {
+    console.log(`HTTP Server is running on port ${HTTP_PORT}`);
+});
+
+https.createServer(options, app).listen(HTTPS_PORT, () => {
+    console.log(`HTTPS Server is running on port ${HTTPS_PORT}`);
 });
